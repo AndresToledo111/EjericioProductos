e78729700ad0571e51925730e91718f1e4594d2a
patri <patri@LAPTOP-J4SPKGO1>
2018-01-31 02:01:08 -0300

    Nuevos datos agregados a una lista

--- src/TestProductos.java
+++ src/TestProductos.java
@@ -30,26 +30,26 @@ public class TestProductos {
         
         
 //        Lista de productos frescos
-//        ProFrescos pf = new ProFrescos("2010", "Chile", "2012", "Doritos", 15);
-////        System.out.println(pf.imprimir());
-//        List<ProFrescos> pfr = new ArrayList<ProFrescos>();
-//        pfr.add(pf);
+        ProFrescos pf = new ProFrescos("2010", "Chile", "2012", "Doritos con arto aire", 15);
+//        System.out.println(pf.imprimir());
+        List<ProFrescos> pfr = new ArrayList<ProFrescos>();
+        pfr.add(pf);
+        
+        System.out.println(pfr);
 //        
-//        System.out.println(pfr);
 //        
-//        
-//
-//        ProRefrigerados pr = new ProRefrigerados(12, "15°C ", "12/12/2012", "Chile", "12/12/2014", "Cereal chocapic", 32);
-//        ProRefrigerados pr1 = new ProRefrigerados(13, "-5°C ", "10/10/2014", "Argentina", "12/12/2018", "Camarones", 12);
-//
 //        //LISTA DE PRODCUTOS REFRIGERADOS
-//        List<ProRefrigerados> pre = new ArrayList<ProRefrigerados>();
-//        pre.add(pr);
-//        pre.add(pr);
-////        pre.add((ProRefrigerados) pf);Entidades.ProFrescos cannot be cast to Entidades.ProRefrigerados
-//        // no se puede guardar en una lista un producto diferente
 //
-//        System.out.println(pre);
+        ProRefrigerados pr = new ProRefrigerados(12, "-5°C ", "12/12/2012", "Chile", "12/12/2014", "arvejas minuto verde", 32);
+        ProRefrigerados pr1 = new ProRefrigerados(13, "-5°C ", "10/10/2014", "Argentina", "12/12/2018", "Camarones cocidos ", 12);
+//
+        List<ProRefrigerados> pre = new ArrayList<ProRefrigerados>();
+        pre.add(pr);
+        pre.add(pr1);
+//        pre.add((ProRefrigerados) pf);Entidades.ProFrescos cannot be cast to Entidades.ProRefrigerados
+        // no se puede guardar en una lista un producto diferente
+
+        System.out.println(pre);
 
     }
 
